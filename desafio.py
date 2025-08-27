89c86274e7549192cab061bf058b9bc6d42fc1ed
Rian Cesar Quintanilha <rianquintanilha5@gmail.com>
2025-08-27 00:30:17 -0300

    Adiciona a classe PessoaFisica com a interface Cliente com propriedades de CPF, nome e data de nascimento

--- desafio.py
+++ desafio.py
@@ -27,3 +27,21 @@ class Cliente:
             Contas: {[conta.numero for conta in self.contas]}
         """
     
+class PessoaFisica(Cliente):
+    def __init__(self, cpf, nome, data_nascimento, endereco):
+        self._cpf = cpf
+        self._nome = nome
+        self._data = data_nascimento
+        super().__init__(endereco)
+
+    @property
+    def cpf(self):
+        return self._cpf
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @property
+    def data(self):
+        return self._data
