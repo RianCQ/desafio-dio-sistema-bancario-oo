1efdef23dc4cb95bf2c663d4bf4e92529a881cc0
Rian Cesar Quintanilha <rianquintanilha5@gmail.com>
2025-08-27 00:46:36 -0300

    Adiciona a classe Deposito que implementa a lógica de registro de depósitos em contas

--- desafio.py
+++ desafio.py
@@ -171,3 +171,19 @@ class Transacao(ABC):
     @abstractmethod
     def registrar(self, conta):
         pass
+
+class Deposito(Transacao):
+    def __init__(self, valor):
+        super().__init__()
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        transacao = conta.depositar(self.valor)
+        
+        if transacao:
+            conta.historico.adicionar_transacao(self)
+
