2c3a55fd959fc05097114a8340987a9399a6bb07
Rian Cesar Quintanilha <rianquintanilha5@gmail.com>
2025-08-27 00:25:54 -0300

    Adiciona a classe Cliente com propriedades de endereço e contas

--- desafio.py
+++ desafio.py
@@ -1,3 +1,17 @@
 from abc import ABC, abstractmethod
 from datetime import datetime
 import textwrap
+
+class Cliente:
+    def __init__(self, endereco):
+        self._endereco = endereco
+        self._contas = []
+    
+    @property
+    def endereco(self):
+        return self._endereco
+    
+    @property
+    def contas(self):
+        return self._contas
+    
