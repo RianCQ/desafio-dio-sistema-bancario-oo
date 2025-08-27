2613687526e5c58474ec74bd9f02d843beebb3c1
Rian Cesar Quintanilha <rianquintanilha5@gmail.com>
2025-08-27 00:47:13 -0300

    Adiciona a classe Saque que implementa a lógica de registro de saques em contas

--- desafio.py
+++ desafio.py
@@ -187,3 +187,17 @@ class Deposito(Transacao):
         if transacao:
             conta.historico.adicionar_transacao(self)
 
+class Saque(Transacao):
+    def __init__(self, valor):
+        self._valor = valor
+
+    @property
+    def valor(self):
+        return self._valor
+    
+    def registrar(self, conta):
+        transacao = conta.sacar(self.valor)
+
+        if transacao:
+            conta.historico.adicionar_transacao(self)
+
